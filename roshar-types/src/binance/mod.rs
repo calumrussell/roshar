6f3a0f35d26bd43ef55a3280ee4eb4945de8746f
calum <calum.mj.russell@gmail.com>
2026-01-24 11:32:24 +0000

    subtask: Add BinancePremiumIndex type for /fapi/v1/premiumIndex API response

--- roshar-types/src/binance/mod.rs
+++ roshar-types/src/binance/mod.rs
@@ -18,3 +18,16 @@ pub struct BinanceHistoricalFundingRate {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub mark_price: Option<String>,
 }
+
+/// Premium index data from Binance Futures API /fapi/v1/premiumIndex
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct BinancePremiumIndex {
+    pub symbol: String,
+    pub mark_price: String,
+    pub index_price: String,
+    pub estimated_settle_price: String,
+    pub last_funding_rate: String,
+    pub next_funding_time: i64,
+    pub interest_rate: String,
+}
